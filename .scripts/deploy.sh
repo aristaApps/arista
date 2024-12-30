4dcad26f009475ce8ae97ab333c3b73e7a4dc7c8
mallawatech <mallawaconnection@gmail.com>
2024-12-30 12:51:56 +0800

    new111

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-set -e
 
-echo "Deployment started ...."
+# Pastikan direktori dan file dimiliki oleh www-data
+echo "Menetapkan kepemilikan direktori dan file ke www-data"
+sudo chown -R www-data:www-data /var/www/arista
 
-# Enter maintenance mode or return true
-# if already is in maintenance mode
-(php artisan down) || true
-npm run build
-# Install composer dependencies
-git reset --hard HEAD
-git pull origin main --no-ff
-composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
-composer dump-autoload
-php artisan cache:clear
-php artisan config:cache
-php artisan config:clear
-php artisan route:cache
-php artisan view:cache
-php artisan storage:link
-# Exit maintenance mode
-php artisan up
+# Mengatur izin akses direktori dan file
+echo "Mengatur permission ke 775 untuk direktori dan file"
+sudo chmod -R 775 /var/www/arista
 
-echo "Deployment finished!"
+# Jalankan build dengan user www-data
+echo "Menjalankan npm install dengan user www-data"
+su -s /bin/bash -c "npm install" www-data
+
+echo "Menjalankan npm run build dengan user www-data"
+su -s /bin/bash -c "npm run build" www-data
+
+# Jalankan maintenance mode dengan user www-data
+echo "Masuk ke maintenance mode"
+sudo -u www-data php artisan down
+
+# Restart aplikasi setelah build selesai
+echo "Menjalankan php artisan up untuk mengaktifkan aplikasi kembali"
+cd /var/www/arista && sudo -u www-data php artisan up
+
+echo "Deployment selesai!"
