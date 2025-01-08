414e7ad509b5ce83d807ecc91f0d851ae52685fe
mallawatech <mallawaconnection@gmail.com>
2025-01-08 12:49:51 +0800

    1

--- app/Http/Controllers/HKTController.php
+++ app/Http/Controllers/HKTController.php
@@ -34,7 +34,7 @@ class HktController extends Controller
     public function create()
     {
         Log::info('Fetching dropdown data for create form');
-        $klasifikasi = Klasifikasi::all();
+        $klasifikasi = Klasifikasi::alll();
         $tingkatPerkembangan = TingkatPerkembangan::all();
         $lokasiArsip = LokasiArsip::all();
         $nasibAkhir = NasibAkhir::all();
