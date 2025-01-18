34aaa70e9f7d5c9df0f2e414f329932ee991b513
mallawatech <mallawaconnection@gmail.com>
2025-01-18 19:36:18 +0800

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -18,41 +18,6 @@
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
-
-        <!-- Nav Item - FAQ -->
-        {{-- <li class="nav-item">
-            <a class="nav-link" href="#" role="button">
-                <i class="fas fa-question-circle fa-fw"></i>
-                <span class="d-none d-lg-inline text-gray-600 small"></span>
-            </a>
-        </li> --}}
-
-        <!-- Nav Item - Alerts (Notifications) -->
-        {{-- <li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-bell fa-fw"></i>
-                <!-- Counter - Alerts -->
-                <span class="badge badge-danger badge-counter">3+</span>
-            </a>
-            <!-- Dropdown - Alerts -->
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-                <h6 class="dropdown-header">Notifications</h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="mr-3">
-                        <div class="icon-circle bg-primary">
-                            <i class="fas fa-file-alt text-white"></i>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="small text-gray-500">December 12, 2024</div>
-                        <span class="font-weight-bold">New report is ready to download!</span>
-                    </div>
-                </a>
-                <!-- Tambahkan notifikasi lainnya di sini -->
-                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-            </div>
-        </li> --}}
-
         <!-- Nav Item - User Logout -->
         <li class="nav-item dropdown no-arrow">
             <form method="POST" action="{{ route('logout') }}" class="nav-link p-0">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,21 +13,32 @@
 
     </head>
     <body class="font-sans antialiased">
+        <!-- Navbar -->
+    <nav class="bg-gray-800 p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <div class="flex items-center">
+                {{-- <img src="{{ asset('sbadmin2/img/kem.png') }}" alt="Gambar Kem" class="w-10 h-10 rounded-lg mr-2"> --}}
+                <img src="{{ asset('sbadmin2/img/more-shadow.png') }}" alt="Gambar More Shadow" class="w-50 h-10 rounded-lg">
+            </div>
+            <div class="space-x-4">
+                <a href="{{ route('login') }}" class="text-white py-2 px-4 rounded-full bg-blue-500 hover:bg-blue-600">Login</a>
+                {{-- <a href="{{ route('register') }}" class="text-white py-2 px-4 rounded-full bg-blue-500 hover:bg-blue-600">Register</a> --}}
+            </div>
+        </div>
+    </nav>
         <div class="relative flex flex-col items-center justify-center min-h-screen">
-            <section class="container w-full px-6 py-6"> <!-- Mengurangi padding -->
+            <section class="container px-6 py-6"> <!-- Mengurangi padding -->
                 <div class="grid gap-6 lg:grid-cols-2">
                     <!-- Kolom 1: Informasi Singkat -->
                     <div class="flex flex-col justify-center p-6 rounded-lg">
                         <!-- Membungkus gambar dalam div flex agar berdampingan dan lebih kecil -->
                         <div class="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-4 mb-4">
-                            <img src="{{ asset('sbadmin2/img/kem.png') }}" alt="Gambar Kem" class="w-30 sm:w-35 rounded-lg">
+                            {{-- <img src="{{ asset('sbadmin2/img/kem.png') }}" alt="Gambar Kem" class="w-30 sm:w-35 rounded-lg"> --}}
                             <img src="{{ asset('sbadmin2/img/more-shadow.png') }}" alt="Gambar More Shadow" class="w-32 sm:w-40 rounded-lg">
                         </div>
                         <h1 class="text-4xl font-extrabold text-center text-gray-800 mb-2">Aplikasi Sistem Terstruktur Arsip LLDIKTI WILAYAH IX</h1>
                         {{-- <h1 class="text-4xl font-extrabold text-gray-800 mb-4"></h1> --}}
                         <p class="text-lg text-center text-gray-600 mb-5">Manajemen file arsip dengan mudah dan cepat</p>
-                        <a href="{{ route('login') }}" class="w-1/2 mx-auto text-center py-3 text-white bg-blue-500 rounded-full hover:bg-blue-600">Login</a>
-                        {{-- <a href="{{ route('register') }}" class="w-1/2 mx-auto text-center py-3 text-white bg-blue-500 rounded-md hover:bg-blue-600">Register</a> --}}
                     </div>
 
                     <!-- Kolom 2: Gambar -->
